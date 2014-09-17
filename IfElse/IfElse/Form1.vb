176c1882675af3a42546a937955b844b1d02f9a2
idsc3101 <idsc3101@gmail.com>
2014-09-17 17:27:48 -0500

    Work Day 2 solved

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -11,9 +11,18 @@ Public Class Form1
         num1 = CDbl(st1)
         num2 = CDbl(st2)
 
-        ' Your code here
+        If num1 < num2 Then
+            txtResult.Text = "The Larger Number Is " & num2
 
-        txtResult.Text = "Not implemented yet"
+        ElseIf num1 < num2 Then
+            txtResult.Text = "they are equal" & num1
+
+        End If
+
+        txtResult.Text = "The Larger number is" & largerNum
     End Sub
 
+    Private Sub txtFirstNum_TextChanged(sender As Object, e As EventArgs) Handles txtFirstNum.TextChanged
+
+    End Sub
 End Class
